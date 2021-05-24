2398975457f79da6819e3f983cdca439ca6171e2
luis felipe <felipe-mara2003@hotmail.com>
2021-05-23 21:04:15 -0300

    help urgeeeente

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import Layout from './components/layout'
+
+import GlobalStyles from './styles/GlobalStyles'
 
 function App() {
   return (
-    <h1>Hello World!</h1>
+    <>
+    <Layout />
+
+    <GlobalStyles />
+    </>
   );
 }
 
---
+++ src/components/layout/styles.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background: var(--primary);
+
+`;
+
+export const Wrapper = styled.div`
+  height: 100%;
+  width: 1200px;
+  margin: 0px auto;
+
+  display: flex;
+  justify-content: center; // centralizando todos itens do nosso layout;
+
+
+
+`;
